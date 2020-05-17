bf01b3aaf481fd7aeed3b10b3f49cd9203b366b4
Sergey Litvinov <slitvinov@gmail.com>
2020-05-17 23:11:45 +0200

    libpetros: no system libs in dep.mk

--- lib/dep.mk
+++ lib/dep.mk
@@ -1,147 +1,74 @@
-acc_theta.o: acc_theta.f95 /usr/include/finclude/math-vector-fortran.h
-acc_x.o: acc_x.f95 /usr/include/finclude/math-vector-fortran.h
-acc_y.o: acc_y.f95 /usr/include/finclude/math-vector-fortran.h
-angle.o: angle.f95 /usr/include/finclude/math-vector-fortran.h
-box9_to_part.o: box9_to_part.f95 \
- /usr/include/finclude/math-vector-fortran.h main_dim.h part.h
-box_1.o: box_1.f95 /usr/include/finclude/math-vector-fortran.h main_dim.h \
- part.h
-box_dim.o: box_dim.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h
-box_to_part.o: box_to_part.f95 \
- /usr/include/finclude/math-vector-fortran.h main_dim.h part.h
-build_tree.o: build_tree.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h tree_tmp.h part.h tree9.h
-ch_to_par.o: ch_to_par.f95 /usr/include/finclude/math-vector-fortran.h
-check_box.o: check_box.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h
-childless9.o: childless9.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h tree9.h
-condiff.o: condiff.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h tree9.h
-diagnos.o: diagnos.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h
-exp_chdless.o: exp_chdless.f95 \
- /usr/include/finclude/math-vector-fortran.h main_dim.h part.h
-far_cls_cyl.o: far_cls_cyl.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h
-far_cls_ubd.o: far_cls_ubd.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h
-gaussian.o: gaussian.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h
-initial.o: initial.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h
-input.o: input.f95 /usr/include/finclude/math-vector-fortran.h main_dim.h \
- measure.h
-int_box.o: int_box.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h
-int_box_part.o: int_box_part.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h
-int_chless1.o: int_chless1.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h main_dim.h part.h \
- tree9.h
-int_chless2.o: int_chless2.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h main_dim.h part.h \
- tree9.h
-int_chless3.o: int_chless3.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h main_dim.h part.h \
- tree9.h
-int_chless4.o: int_chless4.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h main_dim.h part.h \
- tree9.h
-int_chless5.o: int_chless5.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h main_dim.h part.h \
- tree9.h
-int_chless6.o: int_chless6.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h main_dim.h part.h \
- tree9.h
-int_chless7.o: int_chless7.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h main_dim.h part.h \
- tree9.h
-int_chless8.o: int_chless8.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h main_dim.h part.h \
- tree9.h
-int_chless9.o: int_chless9.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h main_dim.h part.h \
- tree9.h
-int_part1.o: int_part1.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h tree_tmp.h
-int_part2.o: int_part2.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h tree_tmp.h part.h
-int_part_box.o: int_part_box.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h
-int_rest2.o: int_rest2.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h main_dim.h part.h tree9.h
-int_rest3.o: int_rest3.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h main_dim.h part.h tree9.h
-int_rest4.o: int_rest4.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h main_dim.h part.h tree9.h
-int_rest5.o: int_rest5.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h main_dim.h part.h tree9.h
-int_rest6.o: int_rest6.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h main_dim.h part.h tree9.h
-int_rest7.o: int_rest7.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h main_dim.h part.h tree9.h
-int_rest8.o: int_rest8.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h main_dim.h part.h tree9.h
-int_rest9.o: int_rest9.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h main_dim.h part.h tree9.h
-make_box.o: make_box.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h
-max_rad.o: max_rad.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h
-mv_ab.o: mv_ab.f95 /usr/include/finclude/math-vector-fortran.h main_dim.h \
- part.h
-mv_eul.o: mv_eul.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h
-mv_rk.o: mv_rk.f95 /usr/include/finclude/math-vector-fortran.h main_dim.h \
- part.h
-near_far.o: near_far.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h
-omega.o: omega.f95 /usr/include/finclude/math-vector-fortran.h
-par_to_ch.o: par_to_ch.f95 /usr/include/finclude/math-vector-fortran.h
-pathlines.o: pathlines.f95 /usr/include/finclude/math-vector-fortran.h
-pot_box_cyl.o: pot_box_cyl.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h
-pot_box_ubd.o: pot_box_ubd.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h
-pot_part_cyl.o: pot_part_cyl.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h
-pot_part_ubd.o: pot_part_ubd.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h
-potential.o: potential.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h
-read_restart.o: read_restart.f95 \
- /usr/include/finclude/math-vector-fortran.h
-remesh.o: remesh.f95 /usr/include/finclude/math-vector-fortran.h \
- remesh_cartesian.h main_dim.h part.h
-str_box.o: str_box.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h
-str_part.o: str_part.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h
-streamfunction.o: streamfunction.f95 \
- /usr/include/finclude/math-vector-fortran.h tree_tmp.h
-update_position.o: update_position.f95 \
- /usr/include/finclude/math-vector-fortran.h
-vel_box.o: vel_box.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h
-vel_ext.o: vel_ext.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h
-vel_part.o: vel_part.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h
-vel_tree.o: vel_tree.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h
-velocity.o: velocity.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h
-velocity_x.o: velocity_x.f95 /usr/include/finclude/math-vector-fortran.h
-velocity_y.o: velocity_y.f95 /usr/include/finclude/math-vector-fortran.h
-vort_field.o: vort_field.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h part.h
-vort_field_ta.o: vort_field_ta.f95 \
- /usr/include/finclude/math-vector-fortran.h main_dim.h part.h measure.h
-wheneq.o: wheneq.f95 /usr/include/finclude/math-vector-fortran.h \
- main_dim.h
-write_restart.o: write_restart.f95 \
- /usr/include/finclude/math-vector-fortran.h
-zeroall.o: zeroall.f95 /usr/include/finclude/math-vector-fortran.h \
- tree_tmp.h main_dim.h tree9.h
+acc_theta.o: acc_theta.f95
+acc_x.o: acc_x.f95
+acc_y.o: acc_y.f95
+angle.o: angle.f95
+box9_to_part.o: box9_to_part.f95 main_dim.h part.h
+box_1.o: box_1.f95 main_dim.h part.h
+box_dim.o: box_dim.f95 main_dim.h part.h
+box_to_part.o: box_to_part.f95 main_dim.h part.h
+build_tree.o: build_tree.f95 main_dim.h tree_tmp.h part.h tree9.h
+ch_to_par.o: ch_to_par.f95
+check_box.o: check_box.f95 tree_tmp.h
+childless9.o: childless9.f95 main_dim.h part.h tree9.h
+condiff.o: condiff.f95 main_dim.h part.h tree9.h
+diagnos.o: diagnos.f95 main_dim.h part.h
+exp_chdless.o: exp_chdless.f95 main_dim.h part.h
+far_cls_cyl.o: far_cls_cyl.f95 tree_tmp.h
+far_cls_ubd.o: far_cls_ubd.f95 tree_tmp.h
+gaussian.o: gaussian.f95 main_dim.h
+initial.o: initial.f95 main_dim.h part.h
+input.o: input.f95 main_dim.h measure.h
+int_box.o: int_box.f95 tree_tmp.h
+int_box_part.o: int_box_part.f95 tree_tmp.h
+int_chless1.o: int_chless1.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_chless2.o: int_chless2.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_chless3.o: int_chless3.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_chless4.o: int_chless4.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_chless5.o: int_chless5.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_chless6.o: int_chless6.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_chless7.o: int_chless7.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_chless8.o: int_chless8.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_chless9.o: int_chless9.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_part1.o: int_part1.f95 main_dim.h tree_tmp.h
+int_part2.o: int_part2.f95 main_dim.h tree_tmp.h part.h
+int_part_box.o: int_part_box.f95 tree_tmp.h
+int_rest2.o: int_rest2.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_rest3.o: int_rest3.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_rest4.o: int_rest4.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_rest5.o: int_rest5.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_rest6.o: int_rest6.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_rest7.o: int_rest7.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_rest8.o: int_rest8.f95 tree_tmp.h main_dim.h part.h tree9.h
+int_rest9.o: int_rest9.f95 tree_tmp.h main_dim.h part.h tree9.h
+make_box.o: make_box.f95 main_dim.h part.h
+max_rad.o: max_rad.f95 main_dim.h part.h
+mv_ab.o: mv_ab.f95 main_dim.h part.h
+mv_eul.o: mv_eul.f95 main_dim.h part.h
+mv_rk.o: mv_rk.f95 main_dim.h part.h
+near_far.o: near_far.f95 tree_tmp.h
+omega.o: omega.f95
+par_to_ch.o: par_to_ch.f95
+pathlines.o: pathlines.f95
+pot_box_cyl.o: pot_box_cyl.f95 tree_tmp.h
+pot_box_ubd.o: pot_box_ubd.f95 tree_tmp.h
+pot_part_cyl.o: pot_part_cyl.f95 tree_tmp.h
+pot_part_ubd.o: pot_part_ubd.f95 tree_tmp.h
+potential.o: potential.f95 tree_tmp.h
+read_restart.o: read_restart.f95
+remesh.o: remesh.f95 remesh_cartesian.h main_dim.h part.h
+str_box.o: str_box.f95 tree_tmp.h
+str_part.o: str_part.f95 tree_tmp.h
+streamfunction.o: streamfunction.f95 tree_tmp.h
+update_position.o: update_position.f95
+vel_box.o: vel_box.f95 tree_tmp.h
+vel_ext.o: vel_ext.f95 main_dim.h part.h
+vel_part.o: vel_part.f95 tree_tmp.h
+vel_tree.o: vel_tree.f95 tree_tmp.h
+velocity.o: velocity.f95 main_dim.h
+velocity_x.o: velocity_x.f95
+velocity_y.o: velocity_y.f95
+vort_field.o: vort_field.f95 main_dim.h part.h
+vort_field_ta.o: vort_field_ta.f95 main_dim.h part.h measure.h
+wheneq.o: wheneq.f95 main_dim.h
+write_restart.o: write_restart.f95
+zeroall.o: zeroall.f95 tree_tmp.h main_dim.h tree9.h
