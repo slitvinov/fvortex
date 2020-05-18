23d9c09a5e09e3d38e63f556840f2edf98cd35cb
Sergey Litvinov <slitvinov@gmail.com>
2020-05-19 00:01:13 +0200

    libpetros: increase np_max

--- lib/tree_tmp.h
+++ lib/tree_tmp.h
@@ -1,6 +1,6 @@
 integer :: nhlp, np_max, nbox_max
 parameter(Nhlp=5000000)
-parameter(np_max=1450000, nbox_max=60001)
+parameter(np_max=3450000, nbox_max=60001)
 integer :: it(np_max)
 real :: Xbox(nbox_max), Ybox(nbox_max), Prbox(nbox_max, 0:7)
 real :: Pibox(nbox_max, 0:7), xt(np_max), yt(np_max), gt(np_max)
