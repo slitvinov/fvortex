a3ee33b426e8f636eb3f72fa3e0e28b8cef45e1f
Sergey Litvinov <slitvinov@gmail.com>
2020-05-26 23:26:33 +0200

    clean up box_dim

--- lib/box_dim.f95
+++ lib/box_dim.f95
@@ -1,13 +1,14 @@
-subroutine box_dim(Num, Xmin, Xmax, Ymin, Ymax)
+subroutine box_dim(Npart, Xmin, Xmax, Ymin, Ymax)
 
 !  This subroutine finds the bounding x and y coordinates in the domain.
 
    implicit none
 
    include 'main_dim.h'
-   include 'part.h'
+   real :: xp(Nvort), yp(Nvort)
+   common/vort1/xp, yp
 
-   integer :: num
+   integer :: Npart
    real :: xmin, xmax, ymin, ymax, x, y
 
    integer :: i
@@ -17,7 +18,7 @@ subroutine box_dim(Num, Xmin, Xmax, Ymin, Ymax)
    Xmax = xp(1)
    Ymin = yp(1)
    Ymax = yp(1)
-   do 10 i = 1, Num
+   do 10 i = 1, Npart
       x = xp(i)
       y = yp(i)
       Xmin = amin1(Xmin, x)
