25bd920c01abd97e02b6cec803bd55c1c2779f9b
Sergey Litvinov <slitvinov@gmail.com>
2021-07-18 13:23:03 +0200

    use ! for comments

--- lib/part.h
+++ lib/part.h
@@ -1,10 +1,13 @@
+!-----------------LOCATIONS --------------------
       real :: xp(nvort), yp(nvort), gp(nvort)
       common/vort1/xp, yp, gp
       real :: xn(nvort), yn(nvort), gn(nvort)
       common/vort2/xn, yn, gn
+!-----------------VELOCITIES --------------------
       real :: uu(nvort), vv(nvort)
       common/vel/uu, vv
       real :: Uold(nvort), Vold(nvort)
       common/oldvel/Uold, Vold
+!-----------------DIFFUSION --------------------
       real :: Gdiff(nvort), GDold(nvort)
       common/diff/Gdiff, GDold
