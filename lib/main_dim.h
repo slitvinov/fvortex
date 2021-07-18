8ffe6ec044bd2af826aa254e3e30d9ed0f8c6a4b
Sergey Litvinov <slitvinov@gmail.com>
2021-07-18 13:15:24 +0200

    headers are for f77

--- lib/main_dim.h
+++ lib/main_dim.h
@@ -1,5 +1,5 @@
-integer :: nvort, ngauss
-real :: gauss_cut
-parameter(nvort=5000000)
-parameter(ngauss=15001)
-parameter(gauss_cut=16.)
+      integer :: nvort, ngauss
+      real :: gauss_cut
+      parameter(nvort=5000000)
+      parameter(ngauss=15001)
+      parameter(gauss_cut=16.)
