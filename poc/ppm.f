130b1e06734f450d8555be55ae0e2de1b9887775
Sergey Litvinov <slitvinov@gmail.com>
2021-07-20 19:12:48 +0200

    dump ppm

--- poc/ppm.f
+++ poc/ppm.f
@@ -1,16 +1,24 @@
+!     gfortran -std=legacy ppm.f
       program main
       implicit none
-      integer m
-      integer n
-      character*1 a
+      integer m, n, k, i, j
+      character zero
+      parameter(m = 200, n = 100, k = 3 * m * n, zero = char(0))
+      character a(3, m, n)
+      data a/k * zero /
 
-c$$$      open(unit=2, file='a.ppm', form='formatted')
-c$$$      m = 600
-c$$$      n = 800
-c$$$      write(2, '(A / I0 X I0 / A)') 'P6', m, n, '255'
-c$$$      close(2)
+      do 10 i = 1, 10
+         do 20 j = 1, n
+            a(1, i, j) = char(255)
+            a(2, i + 10, j) = char(255)
+            a(3, i + 20, j) = char(255)
+ 20      end do
+ 10   end do
 
-      open(unit=2, file='a.ppm', form='unformatted')
+      open(unit=2, file='a.ppm')
+      write(2, '(A / I0 X I0 / A)') 'P6', m, n, '255'
+      close(2)
+      open(unit=2, file='a.ppm', FORM='UNFORMATTED', ACCESS='APPEND')
       write(2) a
       close(2)
       end
