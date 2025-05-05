72e19463f063116c7137526f79939a2f1f729fef
Sergey Litvinov <slitvinov@gmail.com>
2025-05-05 10:05:44 +0200

    up

--- poc/remesh.f
+++ poc/remesh.f
@@ -19,26 +19,12 @@
       real xp(nvort)
       real yp(nvort)
       real gp(nvort)
-      real xn(nvort)
-      real yn(nvort)
-      real gn(nvort)
-      real uu(nvort)
-      real vv(nvort)
-      real Uold(nvort)
-      real Vold(nvort)
-      real Gdiff(nvort)
-      real GDold(nvort)
       common/vort1/xp, yp, gp
-      common/vort2/xn, yn, gn
-      common/vel/uu, vv
-      common/oldvel/Uold, Vold
-      common/diff/Gdiff, GDold
 
       integer np
       real s2
       real ovrlp
-      real gnu
-      common/part/Np, s2, ovrlp, gnu
+      common/part/Np, s2, ovrlp
 
       integer n
       real time
