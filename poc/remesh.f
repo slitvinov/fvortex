944784503d297b0c1ff8b467654dd79c43756a8d
Sergey Litvinov <slitvinov@gmail.com>
2025-05-05 09:52:49 +0200

    up

--- poc/remesh.f
+++ poc/remesh.f
@@ -17,8 +17,6 @@
       integer nvort
       integer nxmaxavg
       integer nymaxavg
-      real gauss_cut
-      parameter(gauss_cut=16.)
       parameter(ngauss=15001)
       parameter(nvort=5000000)
       parameter(nxmaxavg=512)
